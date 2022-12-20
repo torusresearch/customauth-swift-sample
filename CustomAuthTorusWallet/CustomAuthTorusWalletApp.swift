a34db1661d315db6263815978a5ef651d7b1a360
dhruv <dhruv@tor.us>
2022-12-20 11:28:09 +0530

    Initial Commit

---
+++ CustomAuthTorusWallet/CustomAuthTorusWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAuthTorusWalletApp.swift
+//  CustomAuthTorusWallet
+//
+//  Created by Dhruv Jaiswal on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomAuthTorusWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
